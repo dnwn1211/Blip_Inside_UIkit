d515d5ab0e82ce0a8f2e3a54402e24ae267fdfce
jbnong07 <jbnong07@naver.com>
2024-10-24 12:08:26 +0900

    팀 소개, 팀 특징 페이지 구현했습니다!

--- Blip_UI/TeamInfoViewController.swift
+++ Blip_UI/TeamInfoViewController.swift
@@ -20,7 +20,7 @@ class TeamInfoViewController: UIViewController {
         
         // 각 레이블의 yPosition을 설정하여 중앙 정렬
         let yPosition = CGFloat(370) // 첫 번째 레이블의 Y 위치 (로고 아래에 위치)
-
+        
         // 레이블과 이미지를 추가
         let labelsInfo = [
             ("팀 소개", #selector(navigateToTeamIntro)),
@@ -83,23 +83,154 @@ class TeamInfoViewController: UIViewController {
         // 탭 제스처 추가
         let tapGesture = UITapGestureRecognizer(target: self, action: action)
         label.addGestureRecognizer(tapGesture)
-
+        
         return label
     }
-
-
+    
+    
     // 각 레이블 클릭 시 이동할 화면으로의 네비게이션 함수
     @objc func navigateToTeamIntro() {
         let teamIntroVC = UIViewController() // 여기에 실제 팀 소개 화면 추가
-        teamIntroVC.view.backgroundColor = .white
-        teamIntroVC.title = "팀 소개"
+        
+        let backgroundHeight: CGFloat = 100
+        let pageLabel: UILabel = {
+            let label = UILabel()
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.text = "팀 소개"
+            label.font = .boldSystemFont(ofSize: 32)
+            label.textColor = .white
+            
+            return label
+        }()
+        let detailLabel: UILabel = {
+            let label = UILabel()
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.text = "핑거 스냅에서 살아남은 사람들로 구성된 팀 블립은 \n\n전직 최전방 군 간부로 수많은 작전을 성공적으로 수행한 프로젝트 지휘관. 게임과 헬스를 통해 신체와 정신의 한계를 돌파하며, 매 순간 최적의 판단을 내리는 팀플레이의 마에스트로 김석준 클라이밍에 모든 것을 바친 자연의 개척자. \n\n검은색의 세련됨을 즐기며, 소고기로 에너지를 충전해 고난도의 코스를 정복하는 차세대 클라이밍 브랜드 CEO가 될 양정무 \n\n커피 브루잉의 정교함을 즐기는 미식가이자, 강아지 봄이와의 일상을 차분히 책임지는 헌신적인 보호자. 일상 속 면과 고기, 그리고 올리브 그린으로 취향과 감각을 세련되게 표현하는 박진홍 \n\n위 세 명으로 이루어진 3조의 생존자들입니다."
+            label.textColor = .white
+            label.numberOfLines = 0
+            label.lineBreakMode = .byWordWrapping
+            label.font = .systemFont(ofSize: 20)
+            
+            return label
+        }()
+        let titleBackground: UIView = {
+            let background = UIView()
+            background.translatesAutoresizingMaskIntoConstraints = false
+            background.backgroundColor = .titleMain
+            
+            return background
+        }()
+        let detailBorder: UIView = {
+            let border = UIView()
+            border.translatesAutoresizingMaskIntoConstraints = false
+            border.backgroundColor = .clear
+            border.layer.borderColor = UIColor.titleMain.cgColor
+            border.layer.borderWidth = 4
+            
+            return border
+        }()
+        
+        teamIntroVC.view.backgroundColor = .black
+        teamIntroVC.view.addSubview(titleBackground)
+        teamIntroVC.view.addSubview(pageLabel)
+        teamIntroVC.view.addSubview(detailLabel)
+        teamIntroVC.view.addSubview(detailBorder)
+        
+        NSLayoutConstraint.activate([
+            pageLabel.topAnchor.constraint(equalTo: teamIntroVC.view.safeAreaLayoutGuide.topAnchor, constant: backgroundHeight / 2),
+            pageLabel.centerXAnchor.constraint(equalTo: teamIntroVC.view.centerXAnchor),
+            
+            titleBackground.topAnchor.constraint(equalTo: teamIntroVC.view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            titleBackground.heightAnchor.constraint(equalToConstant: backgroundHeight),
+            titleBackground.leadingAnchor.constraint(equalTo: teamIntroVC.view.leadingAnchor),
+            titleBackground.trailingAnchor.constraint(equalTo: teamIntroVC.view.trailingAnchor),
+            titleBackground.centerXAnchor.constraint(equalTo: teamIntroVC.view.centerXAnchor),
+            
+            detailLabel.topAnchor.constraint(equalTo: titleBackground.bottomAnchor, constant: 30),
+            detailLabel.leadingAnchor.constraint(equalTo: teamIntroVC.view.leadingAnchor, constant: 20),
+            detailLabel.trailingAnchor.constraint(equalTo: teamIntroVC.view.trailingAnchor, constant: -20),
+            
+            detailBorder.topAnchor.constraint(equalTo: detailLabel.topAnchor, constant:  -20),
+            detailBorder.leadingAnchor.constraint(equalTo: teamIntroVC.view.leadingAnchor),
+            detailBorder.trailingAnchor.constraint(equalTo: teamIntroVC.view.trailingAnchor),
+            detailBorder.centerXAnchor.constraint(equalTo: teamIntroVC.view.centerXAnchor)
+            
+        ])
+        detailBorder.heightAnchor.constraint(equalTo: detailLabel.heightAnchor, constant: 40).isActive = true
+        
+        
         navigationController?.pushViewController(teamIntroVC, animated: true)
     }
     
     @objc func navigateToFeatures() {
         let featuresVC = UIViewController() // 여기에 실제 특징 화면 추가
-        featuresVC.view.backgroundColor = .white
-        featuresVC.title = "특징"
+        
+        let backgroundHeight: CGFloat = 100
+        let pageLabel: UILabel = {
+            let label = UILabel()
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.text = "팀 특징"
+            label.font = .boldSystemFont(ofSize: 32)
+            label.textColor = .white
+            
+            return label
+        }()
+        let detailLabel: UILabel = {
+            let label = UILabel()
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.text = "10.21\n팀원 6명에서 4명으로 감소\n\n10.21\n팀원 4명에서 1명 합류\n\n10.21\n팀원 5명에서 1명 추가 합류\n\n10.22\n팀원 6명에서 1명 감소\n\n10.22\n팀원 5명에서 1명 추가 감소\n\n10.23\n팀원 4명에서 1명 추가 감소\n\n마침내 팀 이름처럼 절반이 사라지고 3명만 남은 블립 탄생!"
+            label.textColor = .white
+            label.numberOfLines = 0
+            label.lineBreakMode = .byWordWrapping
+            label.font = .systemFont(ofSize: 20)
+            
+            return label
+        }()
+        let titleBackground: UIView = {
+            let background = UIView()
+            background.translatesAutoresizingMaskIntoConstraints = false
+            background.backgroundColor = .titleMain
+            
+            return background
+        }()
+        let detailBorder: UIView = {
+            let border = UIView()
+            border.translatesAutoresizingMaskIntoConstraints = false
+            border.backgroundColor = .clear
+            border.layer.borderColor = UIColor.titleMain.cgColor
+            border.layer.borderWidth = 4
+            
+            return border
+        }()
+        
+        featuresVC.view.backgroundColor = .black
+        featuresVC.view.addSubview(titleBackground)
+        featuresVC.view.addSubview(pageLabel)
+        featuresVC.view.addSubview(detailLabel)
+        featuresVC.view.addSubview(detailBorder)
+        
+        NSLayoutConstraint.activate([
+            pageLabel.topAnchor.constraint(equalTo: featuresVC.view.safeAreaLayoutGuide.topAnchor, constant: backgroundHeight / 2),
+            pageLabel.centerXAnchor.constraint(equalTo: featuresVC.view.centerXAnchor),
+            
+            titleBackground.topAnchor.constraint(equalTo: featuresVC.view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            titleBackground.heightAnchor.constraint(equalToConstant: backgroundHeight),
+            titleBackground.leadingAnchor.constraint(equalTo: featuresVC.view.leadingAnchor),
+            titleBackground.trailingAnchor.constraint(equalTo: featuresVC.view.trailingAnchor),
+            titleBackground.centerXAnchor.constraint(equalTo: featuresVC.view.centerXAnchor),
+            
+            detailLabel.topAnchor.constraint(equalTo: titleBackground.bottomAnchor, constant: 30),
+            detailLabel.leadingAnchor.constraint(equalTo: featuresVC.view.leadingAnchor, constant: 20),
+            detailLabel.trailingAnchor.constraint(equalTo: featuresVC.view.trailingAnchor, constant: -20),
+            
+            detailBorder.topAnchor.constraint(equalTo: detailLabel.topAnchor, constant:  -20),
+            detailBorder.leadingAnchor.constraint(equalTo: featuresVC.view.leadingAnchor),
+            detailBorder.trailingAnchor.constraint(equalTo: featuresVC.view.trailingAnchor),
+            detailBorder.centerXAnchor.constraint(equalTo: featuresVC.view.centerXAnchor)
+            
+        ])
+        detailBorder.heightAnchor.constraint(equalTo: detailLabel.heightAnchor, constant: 40).isActive = true
+        
         navigationController?.pushViewController(featuresVC, animated: true)
     }
     
@@ -109,7 +240,7 @@ class TeamInfoViewController: UIViewController {
         trackGoalsVC.title = "트랙 목표"
         navigationController?.pushViewController(trackGoalsVC, animated: true)
     }
-
+    
     @objc func navigateToPromises() {
         let promisesVC = UIViewController() // 여기에 실제 약속 화면 추가
         promisesVC.view.backgroundColor = .white
@@ -121,5 +252,32 @@ class TeamInfoViewController: UIViewController {
     @objc func navigateToMainContent() {
         let mainContentVC = MainContentViewController()
         navigationController?.pushViewController(mainContentVC, animated: true)
+    }
+}
+
+
+import SwiftUI
+
+struct TeamInfoViewPreview: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> TeamInfoViewController {
+        return TeamInfoViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: TeamInfoViewController, context: Context) {
+        
+    }
+    
+    typealias UIViewControllerType = TeamInfoViewController
+    
+    
+    
+    
+    
+}
+
+@available(iOS 13.0.0, *)
+struct TeamInfoViewUIPreview: PreviewProvider {
+    static var previews: some View {
+        TeamInfoViewPreview()
     }
 }
---
+++ Blip_UI/Test.swift
@@ -0,0 +1,16 @@
+//
+//  Test.swift
+//  Blip_UI
+//
+//  Created by 박진홍 on 10/24/24.
+//
+
+import Testing
+
+struct Test {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
